9d48bb3e723894d691dfe4be7aec41d38098332e
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2021-10-21 16:19:58 +0000

    setting initial static pages

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "sreemoyee.us-south.cf.appdomain.cloud",
+    ]
 
 
 # Application definition
