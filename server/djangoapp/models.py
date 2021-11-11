a6bf4d2f236f4cabb5adfad89aafad7e2fac4400
Sreemoyee Mukherjee <titlisreemoyee@gmail.com>
2021-11-11 19:30:41 +0530

    model migrated

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=60, default="Car Make name")
+    description = models.CharField(null=False, max_length=200, default="Car Make description")
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " + self.name + "," + "Description: " + self.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,7 +26,23 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    TYPE = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'Suv'),
+        (WAGON, 'Wagon'),
+    ]
+    name = models.CharField(null=False, max_length=100, default='Car Model name')
+    dealerId = models.IntegerField()
+    type = models.CharField(max_length=5, choices=TYPE, default=SEDAN)
+    make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    year = models.DateField(default=now)
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " + self.name + "," + "Dealer Id: " + self.dealerId + "," + "Type: " + self.type + "," + "Year: " + self.year + "," + "Car Make: " + self.make
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
